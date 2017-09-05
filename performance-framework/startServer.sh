2dc1cab95cb72b5f08fc76c0d4a8b1fdcfed148d
Ares <root@qa.xingluo>
2017-09-05 19:42:56 +0800

    Modified starServer.sh

--- performance-framework/startServer.sh
+++ performance-framework/startServer.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 killall node
+read -t 5 -p "Do you want to clean the log directory?[y/s]:" result
+if [ "$result" == "y" ];then
+    echo "Try to clean the log directory......"
+    rm -fr ./log/*
+    ls -al ./log/
+else
+    echo "Do nothing......"
+fi
 screen -dmS master node app.js master
